c8158075927174db049f4afab3e30ce00f364ff0
dqt169 <daoquocthai16091999@gmail.com>
2026-09-18 21:05:41 +0700

    Upgrade core to V2

--- app/src/main/java/com/trolytinweb/app/NewsItem.java
+++ app/src/main/java/com/trolytinweb/app/NewsItem.java
@@ -1,10 +1,16 @@
 package com.trolytinweb.app;
 
 public class NewsItem {
-  public final String topic,title,link,source,description;
+  public final String topic,title,link,source,description,origin;
   public final long publishedAt;
+  public String cluster="";
+
   public NewsItem(String topic,String title,String link,String source,String description,long publishedAt){
+    this(topic,title,link,source,description,publishedAt,"Web");
+  }
+
+  public NewsItem(String topic,String title,String link,String source,String description,long publishedAt,String origin){
     this.topic=topic; this.title=title; this.link=link; this.source=source;
-    this.description=description; this.publishedAt=publishedAt;
+    this.description=description; this.publishedAt=publishedAt; this.origin=origin;
   }
 }
